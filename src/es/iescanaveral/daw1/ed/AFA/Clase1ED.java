3c834c65c7767ad2eab76396db9181e0441fc719
Alvaro-FZ <alvarofernandezdelalamo@gmail.com>
2023-12-02 20:19:13 +0100

    Primer commit  de Proyecto1_ED_AFA para la actividad 4.4

---
+++ src/es/iescanaveral/daw1/ed/AFA/Clase1ED.java
@@ -0,0 +1,10 @@
+package es.iescanaveral.daw1.ed.AFA;
+
+public class Clase1ED {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Clase2ED clase2 = new Clase2ED();
+		clase2.saludar("AFA");
+	}
+}
---
+++ src/es/iescanaveral/daw1/ed/AFA/Clase2ED.java
@@ -0,0 +1,8 @@
+package es.iescanaveral.daw1.ed.AFA;
+
+public class Clase2ED {
+	public void saludar(String AFA) {
+		String textoSaludo = "Hola, soy "+AFA+" imprimiendo un mensaje editado con Eclipse";
+		System.out.println(textoSaludo);
+	}
+}
