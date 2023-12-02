4fa6c6e1b92da2a59d54a4c6069dbc2ef5ab2abb
Alvaro-FZ <151030461+Alvaro-FZ@users.noreply.github.com>
2023-12-02 20:27:04 +0100

    Update Clase1ED.java
    Añadido de un comentario desde GitHub

--- src/es/iescanaveral/daw1/ed/AFA/Clase1ED.java
+++ src/es/iescanaveral/daw1/ed/AFA/Clase1ED.java
@@ -4,6 +4,7 @@ public class Clase1ED {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Esto es un comentario incluido desde GitHub
 		Clase2ED clase2 = new Clase2ED();
 		clase2.saludar("AFA");
 	}
